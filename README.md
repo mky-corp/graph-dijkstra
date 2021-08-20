0734df1afcd945d257550b4a494a8f5236495219
Jochizan <remnyachizot2015@gmail.com>
2021-08-20 10:07:15 -0500

    Finish first version graph-dijkstra

--- README.md
+++ README.md
@@ -1,3 +1,45 @@
-# graph-dijkstra
+# [Graph-Dijkstra](https://uncle-liquor.github.io/graph-dijkstra) &middot; [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
-- Aplicación para la búsqueda de la ruta más corta
+## Lenguajes de programación:
+
+| Lenguaje | Descripción |
+| -------- | ----------- |
+| [![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)](https://html5.org/) | Lenguaje de marcados para dar la estructura básica a una página web por estandár |
+| [![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)](https://css-tricks.com/) | Lenguaje en cascada para la estilización de sitios web por estandár |
+| [![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)](https://developer.mozilla.org/es/) | Lenguage de programación que domina el mundo de la web |
+
+## Dependencias de lado del frontend:
+
+| Tecnología | Descripción |
+| ---------- | ----------- |
+| [![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB)](https://es.reactjs.org/) | Framework frontend usado principalmente para la creación de interfaces de usuario |
+| [![Bootstrap](https://img.shields.io/badge/bootstrap-%23563D7C.svg?style=for-the-badge&logo=bootstrap&logoColor=white)](https://getbootstrap.com/) | Framework css para el maquetado fácil de páginas web de todo tipo |
+
+## Otras herramientas
+
+| Herramienta | Descripción | 
+| ----------- | ----------- |
+| [![Visual Studio](https://img.shields.io/badge/VisualStudio-5C2D91.svg?style=for-the-badge&logo=visual-studio&logoColor=white)](https://code.visualstudio.com/) | Editor de código para el desarrollo de software a nivel general |
+| [![Git](https://img.shields.io/badge/git-%23F05033.svg?style=for-the-badge&logo=git&logoColor=white)](https://git-scm.com/) | Control de versiones más usado en todo el mundo del desarrollo de software |
+| [![GitHub](https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white)](https://github.com/) | Plataforma para alojar repositorios locales en la web más extendido y usado en el mundo |
+
+## Contribuir
+
+El propósito principal de este repositorio es continuar evolucionando obteniendo algoritmos core para encontrar la ruta
+más corta, haciéndolo más rápido y fácil de usar. El desarrollo de este repositorio debe ocurrir a la intemperie en
+GitHub, y estamos agradecidos con todo aquel que quiera contribuir con correcciones de errores y mejoras. Lee a
+continuación para saber cómo puedes participar en la mejora de este repositorio.
+
+### [Code of Conduct](./CODE_OF_CONDUCT.md)
+
+Uncle-Liquor ha adoptado un Código de Conducta al que esperamos que los participantes del proyecto se adhieran. Por
+favor leer [CODE OF CONDUCT](./CODE_OF_CONDUCT.md) para que pueda entender qué acciones serán y no serán tolerados
+
+### Guia de contribución
+
+Actualmente no hay una guía de contribución, pero puedes proponer correcciones de errores y mejoras, y cómo construir y
+probar sus cambios en este repositorio.
+
+### License
+
+Graph-Dijkstra is [MIT licensed](./LICENSE).
--- javascript/dijkstra.js
+++ javascript/dijkstra.js
@@ -1,14 +1,49 @@
+/**
+ * @firepower Script para la creación de un grafo que sirve para implementar el algoritmo de dijkstra
+ *
+ * @version 1.0.0
+ *
+ * @license Apache
+ *
+ * @author Jochizan (Roca Hormaza Joan José) <remnyachizot2015@gmail.com>
+ *
+ * History
+ *
+ * v1.0.0 Implementando clases para la implementación de dijkstra
+ *
+ */
+
+
 class Graph {
+
+  /**
+   * Función constructora versión JavaScript para inicializar una instancia de la clase Graph
+   */
   constructor() {
-    this.nodes = [];
-    this.adjacentList = {};
+    this.nodes = []; //  arreglo con los nombres de los nodos
+    this.adjacentList = {}; // objeto con la definición de todas las rutas que existen en el grafo
   }
 
+  /**
+   * Función que añade un nodo a la lista de nodos "nodes" y luego también añade una propiedad con
+   * el nombre que se le paso al objeto "adjacentList" para implementar después las rutas en el grafo.
+   *
+   * @param node{string}
+   */
   addNode(node) {
-    this.nodes.push(node);
-    this.adjacentList[node] = [];
+    this.nodes.push(node); // Añadiendo un nodo al arreglo de nodos
+    this.adjacentList[node] = []; // asignación de un arreglo a partir de un propiedad del objeto adjacentList
   }
 
+  /**
+   * Función para crear una ruta entre un "nodo1" y un "nodo2" asignándole su respectivo "peso" que
+   * hace referencia a la distancia entre los dos nodos o en caso ya exista la ruta solo actualizar
+   * la ruta existente
+   *
+   * @param node1{string}
+   * @param node2{string}
+   * @param weight{number}
+   */
   addEdge(node1, node2, weight) {
     let n1 = this.adjacentList[node1].findIndex((el) => el.node === node2);
     let n2 = this.adjacentList[node2].findIndex((el) => el.node === node1);
@@ -22,81 +57,114 @@ class Graph {
     }
   }
 
+  /**
+   * Función para la búsqueda a través de dos nombres que existan en el arreglo de nodos para la ruta
+   * más corta entre estos dos siempre y cuando ninguno este completamente solo o no ligado al grupo
+   * de conexiones del otro
+   *
+   * @param startNode
+   * @param endNode
+   * @returns {(string[]|number)[]|(*[]|*)[]}
+   */
   dijkstra(startNode, endNode) {
     if (
       !this.adjacentList[startNode].length ||
       !this.adjacentList[endNode].length
-    ) {
-      return [['none'], 0];
-    }
+    )
+      return [['none'], 0]; // esto se retorna si el nodo esta completamente solo
 
-    let times = {};
-    let backtrace = {};
-    let pq = new PriorityQueue();
+    let paths = {}; // objeto que guarda los pesos para hacer las comparaciones
+    let backtrace = {}; // objeto que guarda los nombres para buscar la ruta más corta
+    let pq = new PriorityQueue(); // instancia que crea la cola de prioridad para cada iteración
 
-    times[startNode] = 0;
+    paths[startNode] = 0; // asignamos el valor el peso de 0 para el nodo inicial
     this.nodes.forEach((node) => {
-      if (node !== startNode) {
-        times[node] = Infinity;
-      }
+      if (node !== startNode)
+        paths[node] = Infinity; // asignamos infinito a cada nodo que sea diferente al nodo inicial
     });
 
-    pq.enqueue([startNode, 0]);
+    pq.enqueue([startNode, 0]); // añadimos el primer elemento de la cola prioridad
 
+    // while que se seguirá la iteración siempre y cuando la cola de prioridad no esta vacío
     while (!pq.isEmpty()) {
-      let shortestStep = pq.dequeue();
-      let currentNode = shortestStep[0];
 
-      this.adjacentList[currentNode].forEach((neighbor) => {
-        let time = times[currentNode] + neighbor.weight;
+      let shortestStep = pq.dequeue(); // Nos traemos el nodo que tenga más importancia
+      let currentNode = shortestStep[0]; // Nos traemos el nombre del nodo que tiene más prioridad
 
-        if (time < times[neighbor.node]) {
-          times[neighbor.node] = time;
-          backtrace[neighbor.node] = currentNode;
-          pq.enqueue([neighbor.node, time]);
+      this.adjacentList[currentNode].forEach((neighbor) => { // Iteramos sobre los vecinos del nodo actual
+        let weight = paths[currentNode] + neighbor.weight; // Sumamos le peso actual con el peso del nodo vecino
+
+        if (weight < paths[neighbor.node]) { // si el peso es menor con el valor actual que tiene en los pesos totales
+          paths[neighbor.node] = weight; // asignamos el nuevo valor con ese nodo en las rutas
+          backtrace[neighbor.node] = currentNode; // asignamos el nombre del nodo actual para identificarlos después
+          pq.enqueue([neighbor.node, weight]); // añadimos otro nodo a la cola de prioridad por el peso "weight"
         }
       });
     }
 
-    let path = [endNode];
-    let lastStep = endNode;
+    let path = [endNode]; // creamos la ruta más corta empezando desde el último número
+    let lastStep = endNode; // creamos un valor que comience con el nodo final para iterar en el while
 
     while (lastStep !== startNode) {
-      path.unshift(backtrace[lastStep]);
-      lastStep = backtrace[lastStep];
+      path.unshift(backtrace[lastStep]); // subimos al principio de arreglo los valores del backtrace con el actual "lastStep"
+      lastStep = backtrace[lastStep]; // asignamos el valor del backtrace con el nodo actual seguir obteniendo la ruta más corta
     }
 
-    return [path, times[endNode]];
+    // retornamos un arreglo con dos posiciones que nos devolverá la ruta por nombres en un arreglo
+    // y la distancia total más corta
+    return [path, paths[endNode]];
   }
 }
 
 class PriorityQueue {
+
+  /**
+   * Función constructora para la implementación de una cola de prioridad de dos posiciones
+   * [x, y] -> [string, number]
+   */
   constructor() {
     this.collection = [];
   }
 
+  /**
+   * Función que añade un nodo a la cola de prioridad teniendo en cuenta que el element tiene
+   * dos posiciones que identifican al nodo de la clase Graph y su valor acumulado con respecto al
+   * nodo inicial para luego poder utilizarlo en el algoritmo de dijkstra
+   *
+   * @param element
+   */
   enqueue(element) {
     if (this.isEmpty()) {
       this.collection.push(element);
     } else {
       let added = false;
-      for (let i = 1; i < this.collection.length; ++i) {
-        if (element[1] < this.collection[i - 1][1]) {
+
+      for (let i = 1; i < this.collection.length; ++i) { // iteramos sobre la cola de prioridad
+        if (element[1] < this.collection[i - 1][1]) { // solo si es menor el peso del elemento a alguno de la cola se le asignara en esa ubicación
           this.collection.splice(i - 1, 0, element);
-          added = true;
-          break;
+          added = true; // lo ponemos en true si el "element" tiene un peso menor al que ya existen en la cola
+          break; // forzamos la salida del bucle
         }
       }
       if (!added) {
-        this.collection.push(element);
+        this.collection.push(element); // Solo si "added" es false añadimos el elemento al final cola
       }
     }
   }
 
+  /**
+   * Función que retorna el número inicial del arreglo [x, y, z] sale -> x
+   *
+   * @returns {number}
+   */
   dequeue() {
     return this.collection.shift();
   }
 
+  /**
+   * Función que retorna un boolean que identifica si la cola esta vacía
+   * @returns {boolean}
+   */
   isEmpty() {
     return this.collection.length === 0;
   }
